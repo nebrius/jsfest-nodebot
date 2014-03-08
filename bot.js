4b3946c127adfd95a925b8d120b48ca16b773682
Bryan Hughes <bryan@theoreticalideations.com>
2014-03-08 14:59:50 -0800

    Final code

--- bot.js
+++ bot.js
@@ -1,10 +1,18 @@
 var five = require('johnny-five'),
-    board = new five.Board();
+    board = new five.Board({
+      repl: false
+    });
 
 var LEFT_FORWARD = 180;
+var LEFT_FORWARD_SLOW = 92.5;
 var RIGHT_FORWARD = 0;
+var RIGHT_FORWARD_SLOW = 87.5;
 var LEFT_REVERSE = 0;
+var LEFT_REVERSE_SLOW = 87.5;
 var RIGHT_REVERSE = 180;
+var RIGHT_REVERSE_SLOW = 92.5;
+var CLAW_OPEN = 80;
+var CLAW_CLOSED = 100;
 
 board.on('ready', function() {
 
@@ -18,8 +26,15 @@ board.on('ready', function() {
     type: 'continuous'
   });
 
+  var clawServo = new five.Servo(12);
+
+  leftServo.stop();
+  rightServo.stop();
+  clawServo.to(CLAW_OPEN);
+
   var control = require('./control');
   control.on('move', function (direction) {
+    console.log('moving: ' + direction);
     switch(direction) {
     case 'none':
       leftServo.stop();
@@ -31,7 +46,7 @@ board.on('ready', function() {
       break;
     case 'upright':
       leftServo.to(LEFT_FORWARD);
-      rightServo.stop();
+      rightServo.to(RIGHT_FORWARD_SLOW);
       break;
     case 'right':
       leftServo.to(LEFT_FORWARD);
@@ -39,14 +54,14 @@ board.on('ready', function() {
       break;
     case 'downright':
       leftServo.to(LEFT_REVERSE);
-      rightServo.stop();
+      rightServo.to(RIGHT_REVERSE_SLOW);
       break;
     case 'down':
       leftServo.to(LEFT_REVERSE);
       rightServo.to(RIGHT_REVERSE);
       break;
     case 'downleft':
-      leftServo.stop();
+      leftServo.to(LEFT_REVERSE_SLOW);
       rightServo.to(RIGHT_REVERSE);
       break;
     case 'left':
@@ -54,10 +69,20 @@ board.on('ready', function() {
       rightServo.to(RIGHT_FORWARD);
       break;
     case 'upleft':
-      leftServo.stop();
+      leftServo.to(LEFT_FORWARD_SLOW);
       rightServo.to(RIGHT_FORWARD);
       break;
     }
+  });
+
+  control.on('close', function() {
+    console.log('closing');
+    clawServo.to(CLAW_CLOSED);
+  });
+
+  control.on('open', function() {
+    console.log('opening');
+    clawServo.to(CLAW_OPEN);
   });
 
   console.log('ready!');
--- control.js
+++ control.js
@@ -7,7 +7,16 @@ http.createServer(function (req, res) {
   var url = require('url').parse(req.url);
   var set = url.path.match(/^\/set\/(.*)$/);
   if (set) {
+    console.log('move', set[1]);
     evt.emit('move', set[1]);
+    res.end();
+  } else if (url.path == '/close') {
+    console.log('close');
+    evt.emit('close');
+    res.end();
+  } else if (url.path == '/open') {
+    console.log('open');
+    evt.emit('open');
     res.end();
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
